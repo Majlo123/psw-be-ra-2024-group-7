d9d0162c700f65b67bb6b2eede30a6fd6609bcda
Momcilo02 <mocadj2002@gmail.com>
2024-10-10 16:07:08 +0200

    feat: Add Blog Class

---
+++ src/Modules/Blog/Explorer.Blog.Core/Domain/Blog.cs
@@ -0,0 +1,46 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Blog.Core.Domain
+{
+    public class Blog : Entity
+    {
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public BlogStatus Status { get; set; }
+        public List<string> ImageUrl { get; set; }
+        public DateOnly Date {  get; set; }
+
+        public Blog(int blogId, string title, string description, BlogStatus status, List<string> imageUrl, DateOnly date)
+        {
+            BlogId = blogId;
+            Title = title;
+            Description = description;
+            Status = status;
+            ImageUrl = imageUrl;
+            Date = date;
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (BlogId == 0) throw new ArgumentException("Invalid BlogId");
+            if(string.IsNullOrWhiteSpace(Title)) throw new ArgumentException("Invalid title");
+            if(string.IsNullOrWhiteSpace(Description)) throw new ArgumentException("Invalid description");
+
+        }
+    }
+
+    public enum BlogStatus
+    {
+        draft,
+        published,
+        closed
+    }
+}

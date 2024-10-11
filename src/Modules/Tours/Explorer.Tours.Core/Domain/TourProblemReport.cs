c8e03b28208e2dfc29af2e253b28fe0d9aee4150
Isidora Pavlovic <isidora.pavlovic123@gmail.com>
2024-10-11 19:28:21 +0200

    Created domain class TourProblemReport

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourProblemReport.cs
@@ -0,0 +1,48 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public enum ProblemPriority
+    {
+        LOW,    
+        MEDIUM, 
+        HIGH   
+    }
+    public class TourProblemReport : Entity
+    {
+        public int TourId { get; private set; }
+        public string Category { get; private set; }
+        public ProblemPriority Priority { get; private set; }
+        public string Description { get; private set; }
+        public DateTime Time { get; private set; }
+
+        public TourProblemReport(int tourId, string category, ProblemPriority priority, string description, DateTime time)
+        {
+            if (tourId <= 0)
+                throw new ArgumentException("TourId must be greater than 0", nameof(tourId));
+
+            if (string.IsNullOrWhiteSpace(category))
+                throw new ArgumentException("Category cannot be empty", nameof(category));
+
+            if (!Enum.IsDefined(typeof(ProblemPriority), priority)) 
+                throw new ArgumentException("Invalid priority value", nameof(priority));
+
+            if (string.IsNullOrWhiteSpace(description))
+                throw new ArgumentException("Description cannot be empty", nameof(description));
+
+            if (time > DateTime.Now)
+                throw new ArgumentException("Time cannot be in the future", nameof(time));
+
+            TourId = tourId;
+            Category = category;
+            Priority = priority;
+            Description = description;
+            Time = time;
+        }
+    }
+}

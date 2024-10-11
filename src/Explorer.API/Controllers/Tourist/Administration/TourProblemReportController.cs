f6a0b6a4b43192341e56818cadae264abfe1d560
Isidora Pavlovic <isidora.pavlovic123@gmail.com>
2024-10-11 20:22:32 +0200

    feat: Created controller.

---
+++ src/Explorer.API/Controllers/Tourist/Administration/TourProblemReportController.cs
@@ -0,0 +1,48 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Tourist.Administration
+{
+    [Authorize(Policy = "touristPolicy")]
+    [Route("api/administration/tour-problem-report")]
+    public class TourProblemReportController : BaseApiController
+    {
+        private readonly ITourProblemReportService _tourProblemReportService;
+
+        public TourProblemReportController(ITourProblemReportService tourProblemReportService)
+        {
+            _tourProblemReportService = tourProblemReportService;
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<TourProblemReportDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _tourProblemReportService.GetPaged(page, pageSize);
+            return CreateResponse(result);
+        }
+
+        [HttpPost]
+        public ActionResult<TourProblemReportDto> Create([FromBody] TourProblemReportDto tourProblemReport)
+        {
+            var result = _tourProblemReportService.Create(tourProblemReport);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<TourProblemReportDto> Update([FromBody] TourProblemReportDto tourProblemReport)
+        {
+            var result = _tourProblemReportService.Update(tourProblemReport);
+            return CreateResponse(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var result = _tourProblemReportService.Delete(id);
+            return CreateResponse(result);
+        }
+    }
+}

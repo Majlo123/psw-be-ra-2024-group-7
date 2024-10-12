a1033033631b1c9f059bf366e1c5f0e22e43a871
lnosovic <149266103+lnosovic@users.noreply.github.com>
2024-10-12 15:45:04 +0200

    feat: Created TouristEquipment model and dto, implemented service and mapper

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/TouristEquipment.cs
@@ -0,0 +1,29 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.Core.Domain
+{
+    public class TouristEquipment : Entity
+    {
+        public long TouristId { get; init; }
+        public int EquipmentId { get; init; }
+        public TouristEquipment(long touristId, int equipmentId)
+        {
+            TouristId = touristId;
+            EquipmentId = equipmentId;
+            Validate();
+        }
+        public void Validate()
+        {
+            if (EquipmentId <= 0)
+                throw new ArgumentException("TourId must be greater than 0", nameof(EquipmentId));
+            if(TouristId<= 0)
+                throw new ArgumentException("TourId must be greater than 0", nameof(TouristId));
+
+        }
+    }
+}

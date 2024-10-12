49ed45f1a51f2de157c52725e6b862758c55b1ed
Momcilo02 <mocadj2002@gmail.com>
2024-10-12 13:54:17 +0200

    test: Blog Create And Get
    Ima i promena na drugim klasamo posto sam imao problem sa bazom zbog Id

--- src/Modules/Blog/Explorer.Blog.API/Dtos/BlogDto.cs
+++ src/Modules/Blog/Explorer.Blog.API/Dtos/BlogDto.cs
@@ -8,7 +8,7 @@ namespace Explorer.Blog.API.Dtos
 {
     public class BlogDto
     {
-        public int BlogId { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
 
--- src/Modules/Blog/Explorer.Blog.Core/Domain/Blog.cs
+++ src/Modules/Blog/Explorer.Blog.Core/Domain/Blog.cs
@@ -9,29 +9,25 @@ namespace Explorer.Blog.Core.Domain
 {
     public class Blog : Entity
     {
-        public int BlogId { get;private set; }
-        public string Title { get; private set; }
-        public string Description { get; private set; }
+        public string Title { get; init; }
+        public string Description { get; init; }
 
-        public BlogStatus Status { get; private set; }
-        public List<string> ImageUrl { get; private set; }
-        public DateOnly Date {  get; private set; }
+        public BlogStatus Status { get; init; }
+        public List<string> ImageUrl { get; init; }
+        public DateOnly Date {  get; init; }
 
-        public Blog(int blogId, string title, string description, BlogStatus status, List<string> imageUrl, DateOnly date)
+        public Blog(string title, string description, BlogStatus status, List<string> imageUrl, DateOnly date)
         {
-            Validate();
-            BlogId = blogId;
             Title = title;
             Description = description;
             Status = status;
             ImageUrl = imageUrl;
             Date = date;
-            
+            Validate();
         }
 
         private void Validate()
         {
-            if (BlogId == 0) throw new ArgumentException("Invalid BlogId");
             if(string.IsNullOrWhiteSpace(Title)) throw new ArgumentException("Invalid title");
             if(string.IsNullOrWhiteSpace(Description)) throw new ArgumentException("Invalid description");
 

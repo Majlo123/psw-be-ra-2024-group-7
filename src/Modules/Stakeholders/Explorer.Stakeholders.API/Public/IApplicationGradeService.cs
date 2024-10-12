a9d602c3d83b7535885028d5385274e991b788e3
AleksandarSekulic1 <149141670+AleksandarSekulic1@users.noreply.github.com>
2024-10-12 16:06:19 +0200

    feat: implementation of application rating system

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Public/IApplicationGradeService.cs
@@ -0,0 +1,20 @@
+﻿using FluentResults;
+using Explorer.Stakeholders.API.Dtos;
+
+namespace Explorer.Stakeholders.API.Public
+{
+    public interface IApplicationGradeService
+    {
+        Result<ApplicationGradeDto> AddGrade(ApplicationGradeDto applicationGrade);
+        Result<PagedResult<ApplicationGradeDto>> GetGrades(int page, int pageSize);  // Ova metoda vraća PagedResult
+        Result<bool> UserExists(int userId);
+    }
+
+    public class PagedResult<T>
+    {
+        public List<T> Items { get; set; }
+        public int TotalCount { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+    }
+}

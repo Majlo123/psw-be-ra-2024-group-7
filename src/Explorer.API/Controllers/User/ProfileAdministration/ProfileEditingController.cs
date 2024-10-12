3b3ee7c676a37a29bfe18207aa9a47b9d7e058cb
Mihajlo Bogdanovic <bogdanovicmihajlo68@gmail.com>
2024-10-12 04:26:16 +0200

    feat: configured repository and dependency injection for UserProfile

---
+++ src/Explorer.API/Controllers/User/ProfileAdministration/ProfileEditingController.cs
@@ -0,0 +1,33 @@
+﻿using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.User.ProfileAdministration
+{
+    [Authorize(Policy = "userPolicy")]
+    [Route("api/profile-administration/edit")]
+    public class ProfileEditingController : BaseApiController
+    {
+        private readonly IPersonEditingService _personEditingService;
+
+        public ProfileEditingController(IPersonEditingService personEditingService)
+        {
+            _personEditingService = personEditingService;
+        }
+
+        [HttpPut]
+        public ActionResult<PersonDto> Edit([FromForm] PersonDto person)
+        {
+            var result = _personEditingService.Update(person);
+            return CreateResponse(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<PersonDto> GetUserInfo(int id)
+        {
+            var result = _personEditingService.Get(id);
+            return CreateResponse(result);
+        }
+    }
+}
--- src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/StakeholdersStartup.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/StakeholdersStartup.cs
@@ -25,6 +25,8 @@ public static class StakeholdersStartup
     
     private static void SetupCore(IServiceCollection services)
     {
+        services.AddScoped<IPersonEditingService, PersonEditingService>();
+        services.AddScoped<IPersonRepository, PersonDatabaseRepository>();
         services.AddScoped<IAuthenticationService, AuthenticationService>();
         services.AddScoped<ITokenGenerator, JwtGenerator>();
     }
@@ -33,7 +35,7 @@ public static class StakeholdersStartup
     {
         services.AddScoped(typeof(ICrudRepository<Person>), typeof(CrudDatabaseRepository<Person, StakeholdersContext>));
         services.AddScoped<IUserRepository, UserDatabaseRepository>();
-
+        services.AddScoped<IPersonRepository, PersonDatabaseRepository>();
         services.AddDbContext<StakeholdersContext>(opt =>
             opt.UseNpgsql(DbConnectionStringBuilder.Build("stakeholders"),
                 x => x.MigrationsHistoryTable("__EFMigrationsHistory", "stakeholders")));

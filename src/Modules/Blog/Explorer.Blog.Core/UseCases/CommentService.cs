fe3effd56108bb0d275655a35a3f028905718518
markobijelic1 <141180314+markobijelic1@users.noreply.github.com>
2024-10-14 19:16:41 +0200

    feat:added service model and dto

---
+++ src/Modules/Blog/Explorer.Blog.Core/UseCases/CommentService.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Explorer.Blog.API.Dtos;
+using Explorer.Blog.API.Public;
+using Explorer.Blog.Core.Domain;
+using Explorer.BuildingBlocks.Core.UseCases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Blog.Core.UseCases
+{
+    //public class EquipmentService : CrudService<EquipmentDto, Equipment>, IEquipmentService
+    //{
+    //    public EquipmentService(ICrudRepository<Equipment> repository, IMapper mapper) : base(repository, mapper) { }
+    //}
+    public class CommentService : CrudService <CommentDto,Comment>,ICommentService
+    {
+        public CommentService(ICrudRepository<Comment> repository, IMapper mapper) : base(repository, mapper) { }
+    }
+}

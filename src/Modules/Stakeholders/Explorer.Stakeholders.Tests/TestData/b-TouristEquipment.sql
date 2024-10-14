3efb89f3109f561322a20f7b54f16de13e4ee568
lnosovic <149266103+lnosovic@users.noreply.github.com>
2024-10-14 12:47:04 +0200

    test: Implemented tests for TouristEquipment

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/b-TouristEquipment.sql
@@ -0,0 +1,9 @@
+INSERT INTO stakeholders."TouristEquipments"(
+"Id", "TouristId", "EquipmentId")
+VALUES (-0, 1, 1);
+INSERT INTO stakeholders."TouristEquipments"(
+"Id", "TouristId", "EquipmentId")
+VALUES (-1, 1, 2);
+INSERT INTO stakeholders."TouristEquipments"(
+"Id", "TouristId", "EquipmentId")
+VALUES (-2, 2, 1);

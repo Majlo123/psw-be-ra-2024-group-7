7bbf0e549ea0d3e8e402ddc86b2a422b16114ff1
markobijelic1 <141180314+markobijelic1@users.noreply.github.com>
2024-10-14 22:10:43 +0200

    feat/tests added

---
+++ src/Modules/Blog/Explorer.Blog.Tests/Integration/CommentCommandTest.cs
@@ -0,0 +1,127 @@
+﻿using Explorer.API.Controllers.User;
+using Explorer.Blog.API.Dtos;
+using Explorer.Blog.API.Public;
+using Explorer.Blog.Core.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Explorer.Blog.API.Dtos;
+using Explorer.Blog.API.Public;
+using Explorer.Blog.Infrastructure.Database;
+using Explorer.Blog.Core.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace Explorer.Blog.Tests.Integration
+{
+    [Collection("Sequential")]
+    public class CommentCommandTest : BaseBlogIntegrationTest
+    {
+        public CommentCommandTest(BlogTestFactory factory) : base(factory) { }
+
+        [Fact]
+        public void CreatesComment()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var dbContext = scope.ServiceProvider.GetRequiredService<BlogContext>();
+            var newComment = new CommentDto
+            {
+                UserId = 1,  // Korisnički ID je integer
+                Text = "This is a test comment",
+                CreatedAt = DateTime.UtcNow
+            };
+
+            // Act
+            var result = ((ObjectResult)controller.Create(newComment).Result)?.Value as CommentDto;
+
+            // Assert - Response
+            result.ShouldNotBeNull();
+            result.UserId.ShouldBe(newComment.UserId);
+            result.Text.ShouldBe(newComment.Text);
+
+            // Assert - Database
+            var storedEntity = dbContext.Comments.FirstOrDefault(i => i.Text == newComment.Text);
+            storedEntity.ShouldNotBeNull();
+            storedEntity.Id.ShouldBe(result.Id);
+            storedEntity.UserId.ShouldBe(newComment.UserId);  // Proveravamo UserId
+        }
+
+        [Fact]
+        public void UpdatesComment()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var dbContext = scope.ServiceProvider.GetRequiredService<BlogContext>();
+
+            var existingComment = new Comment(1, "Original comment text");  // Kreiramo komentar sa UserId = 1
+            dbContext.Comments.Add(existingComment);
+            dbContext.SaveChanges();
+
+            var updatedComment = new CommentDto
+            {
+                Id = (int)existingComment.Id,
+                UserId = 2,  // Ažuriramo UserId na 2
+                Text = "Updated comment text",
+                CreatedAt = existingComment.CreatedAt,
+                LastModified = DateTime.UtcNow
+            };
+
+            // Act
+            var result = ((ObjectResult)controller.Update(updatedComment).Result)?.Value as CommentDto;
+
+            // Assert - Response
+            result.ShouldNotBeNull();
+            result.UserId.ShouldBe(updatedComment.UserId);  // Proveravamo ažurirani UserId
+            result.Text.ShouldBe(updatedComment.Text);
+
+            // Assert - Database
+            var storedEntity = dbContext.Comments.FirstOrDefault(i => i.Id == existingComment.Id);
+            storedEntity.ShouldNotBeNull();
+            storedEntity.UserId.ShouldBe(updatedComment.UserId);  // Proveravamo ažurirani UserId
+            storedEntity.Text.ShouldBe(updatedComment.Text);
+        }
+
+        [Fact]
+        public void DeletesComment()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var dbContext = scope.ServiceProvider.GetRequiredService<BlogContext>();
+
+            var existingComment = new Comment(1, "Comment to delete");  // Kreiramo komentar sa UserId = 1
+            dbContext.Comments.Add(existingComment);
+            dbContext.SaveChanges();
+
+            // Act
+            var result = controller.Delete((int)existingComment.Id).ExecuteResult;
+
+            // Assert - Response
+            result.ShouldBeOfType<OkResult>();
+
+            // Assert - Database
+            var deletedEntity = dbContext.Comments.FirstOrDefault(i => i.Id == existingComment.Id);
+            deletedEntity.ShouldBeNull();
+        }
+
+        private static CommentController CreateController(IServiceScope scope)
+        {
+            return new CommentController(scope.ServiceProvider.GetRequiredService<ICommentService>())
+            {
+                ControllerContext = BuildContext("-1")
+            };
+        }
+    }
+}

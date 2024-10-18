5a04c4ca58e2f4880c6ba115441bd2ab598b81a8
MilosNesh <neskovic.milos02@gmail.com>
2024-10-18 14:32:04 +0200

    feat: implemented image storage and loading

--- src/Explorer.API/Controllers/Author/KeyPointController.cs
+++ src/Explorer.API/Controllers/Author/KeyPointController.cs
@@ -18,7 +18,7 @@ public class KeyPointController : BaseApiController
 
     [HttpPost]
     public ActionResult<KeyPointDto> Create([FromBody] KeyPointDto keyPointDto)
-    {
+        {
         var result = _keyPointService.Create(keyPointDto);
         return CreateResponse(result);
     }
@@ -28,5 +28,35 @@ public class KeyPointController : BaseApiController
     {
         var result = _keyPointService.GetAll();
         return CreateResponse(result);
+    }
+
+    [HttpPost]
+    [Route("image")]
+    public async Task<IActionResult> Upload([FromForm] IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+            return BadRequest("No file uploaded.");
+
+        var path = Path.Combine("C:\\Users\\PC\\Desktop", file.FileName);
+
+        using (var stream = new FileStream(path, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        return Ok(new { filePath = path });
+    }
+
+    [HttpGet]
+    [Route("image")]
+    public IActionResult GetImage([FromQuery] string filePath)
+    {
+        if (!System.IO.File.Exists(filePath))
+        {
+            return NotFound();
+        }
+
+        var fileBytes = System.IO.File.ReadAllBytes(filePath);
+        return File(fileBytes, "image/jpeg");
     }
 }

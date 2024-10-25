48991c2fad798d9e10773d994c200e74d8dc420e
Mihajlo Bogdanovic <bogdanovicmihajlo68@gmail.com>
2024-10-25 19:27:31 +0200

    feat:Shopping cart agregate added

---
+++ src/Modules/Shopping/Explorer.Shopping.Infrastructure/Database/ShoppingContext.cs
@@ -0,0 +1,28 @@
+﻿using Explorer.Shopping.Core.Domain.ShoppingCarts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Explorer.Tours.Infrastructure.Database;
+
+public class ShoppingContext : DbContext
+{
+    public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    public DbSet<TourPurchaseToken> TourPurchaseTokens { get; set; }
+
+    public ShoppingContext(DbContextOptions<ShoppingContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("tours");
+        modelBuilder.Entity<ShoppingCart>()
+           .Property(cart => cart.items)
+           .HasColumnType("jsonb");
+        ConfigureShoppingCart(modelBuilder);
+    }
+
+    private static void ConfigureShoppingCart(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TourPurchaseToken>()
+            .HasIndex(tpt => new { tpt.UserId, tpt.TourId })
+            .IsUnique();
+    }
+}

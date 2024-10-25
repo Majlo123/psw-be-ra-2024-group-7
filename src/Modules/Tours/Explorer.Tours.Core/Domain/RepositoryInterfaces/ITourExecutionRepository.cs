8960133606376e34ba83391d8fd829373484c99a
Srxx42 <srdjan.vukojevic002@gmail.com>
2024-10-25 23:44:21 +0200

    Implemented TourExecution aggregate with Domain, Repository and RepInterface

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/RepositoryInterfaces/ITourExecutionRepository.cs
@@ -0,0 +1,16 @@
+﻿using Explorer.Tours.Core.Domain.TourExecutions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain.RepositoryInterfaces
+{
+    public interface ITourExecutionRepository
+    {
+        //Ovde unosimo metode potrebne za rad sa TourExecution
+       
+
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourExecutions/CompletedKeyPoints.cs
@@ -0,0 +1,38 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain.TourExecutions
+{
+    public class CompletedKeyPoints : ValueObject
+    {
+        public KeyPoint CompletedKeyPoint { get; private set; }
+        public DateTime ExecutionTime { get; private set; }
+        public int TouristId { get; private set; }
+        public int TourId { get; private set; }
+
+        [JsonConstructor]
+        public CompletedKeyPoints(KeyPoint keyPoint, DateTime executionTime, int touristId, int tourId)
+        {
+            CompletedKeyPoint = keyPoint;
+            ExecutionTime = executionTime;
+            TouristId = touristId;
+            TourId = tourId;
+        }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return CompletedKeyPoint;
+            yield return ExecutionTime;
+            yield return TouristId;
+            yield return TourId;
+        }
+
+ 
+    }
+}

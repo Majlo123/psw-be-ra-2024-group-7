b16342121c96dc43535a6042b0559e6dedd15e9a
Isidora Pavlovic <isidora.pavlovic123@gmail.com>
2024-10-25 16:49:58 +0200

    feat: Implemented aggregate for TourIssueLifecycle

--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/RepositoryInterfaces/ITourProblemReportRepository.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/RepositoryInterfaces/ITourProblemReportRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Explorer.Stakeholders.Core.Domain.TourProblemReports;
 
 namespace Explorer.Stakeholders.Core.Domain.RepositoryInterfaces
 {

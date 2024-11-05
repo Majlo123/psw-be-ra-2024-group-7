44f528d3932c9dda9bdf4c0eadf10e8c0e35d140
MilosNesh <neskovic.milos02@gmail.com>
2024-11-05 16:01:30 +0100

    fix: show published tours tests

--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/Integration/Administration/TourQueryTests.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/Integration/Administration/TourQueryTests.cs
@@ -28,12 +28,12 @@ public class TourQueryTests : BaseStakeholdersIntegrationTest
         var controller = CreateController(scope);
 
         //Act
-        var result = ((ObjectResult)controller.GetPublished(0, 0).Result).Value as PagedResult<BasicTourDetailsDto>;
+        var result = ((ObjectResult)controller.GetPublished(0, 0).Result).Value as PagedResult<TourDto>;
 
         //Assert
         result.ShouldNotBeNull();
-        result.Results.Count.ShouldBe(4);
-        result.TotalCount.ShouldBe(4);
+        result.Results.Count.ShouldBe(1);
+        result.TotalCount.ShouldBe(1);
     }
     private static TourController CreateController(IServiceScope scope)
     {

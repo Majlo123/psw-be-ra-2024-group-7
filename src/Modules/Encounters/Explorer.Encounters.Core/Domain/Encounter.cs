a08faeca63dd85e41287e2fcd922bb41310aab97
Srxx42 <srdjan.vukojevic002@gmail.com>
2024-11-16 20:10:25 +0100

    Can't ontinue implementing, dont know if i need 3 or 1 VO

---
+++ src/Modules/Encounters/Explorer.Encounters.Core/Domain/Encounter.cs
@@ -0,0 +1,58 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.Core.Domain
+{
+    public enum EncounterStatus
+    {
+        ACTIVE,
+        DRAFT,
+        ARCHIEVED,
+        DELETED
+    }
+
+    public enum EncounterType
+    {
+        SOCIAL,
+        LOCATION,
+        MISC
+    }
+    public enum TouristEncounterStatus
+    {
+        ACCEPTED,
+        AWAITS,
+        CANCELLED
+    }
+    public class Encounter : Entity
+    {
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+        public int Total_xp { get; private set; }
+
+        public int CreatorId { get; private set; }
+
+        public double longitude { get; private set; }
+        public double latitude { get; private set; }
+
+        public EncounterStatus Status { get; private set;}
+
+        public EncounterType Type { get; private set; }
+
+        public TouristEncounterStatus  TouristStatus { get; private set; }
+
+        public bool isTourRequired { get; private set; }
+
+        public int tourId { get; private set; }
+
+        // Da li 3 ili 1 VO??
+
+
+
+
+    }
+}

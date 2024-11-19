05aaac033abdb61749958cd74e3ab101ea218690
Mihajlo Bogdanovic <bogdanovicmihajlo68@gmail.com>
2024-11-19 02:37:56 +0100

    feat:Wallet controller added

---
+++ src/Explorer.API/Controllers/Tourist/Shopping/TouristWalletController.cs
@@ -0,0 +1,40 @@
+﻿using Explorer.Shopping.API.Dtos;
+using Explorer.Shopping.API.Public;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Tourist.Shopping
+{
+    [Authorize(Policy = "administratorAndTouristPolicy")]
+    [Route("api/tourist/wallet")]
+    public class TouristWalletController : BaseApiController
+    {
+        private readonly ITouristWalletService _service;
+
+        public TouristWalletController(ITouristWalletService touristWalletService)
+        {
+            _service = touristWalletService;
+        }
+
+        [HttpGet("get-adventure-coins/{userId:long}")]
+        public ActionResult<TouristWalletDto> GetAdventureCoins(int userId)
+        {
+            var result = _service.GetAdventureCoins(userId);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("payment-adventure-coins/{userId:long}/{adventureCoins:int}")]
+        public ActionResult<TouristWalletDto> PaymentAdventureCoins(int userId, int adventureCoins)
+        {
+            var result = _service.PaymentAdventureCoins(userId, adventureCoins);
+            return CreateResponse(result);
+        }
+
+        [HttpPost("create-wallet")]
+        public ActionResult<TouristWalletDto> GetAdventureCoins([FromBody] TouristWalletDto touristWalletDto)
+        {
+            var result = _service.Create(touristWalletDto);
+            return CreateResponse(result);
+        }
+    }
+}

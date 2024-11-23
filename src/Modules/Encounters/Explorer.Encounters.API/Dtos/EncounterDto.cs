99e0719de8ee72f9e919280f2671ec60fb973659
Isidora Pavlovic <isidora.pavlovic123@gmail.com>
2024-11-23 22:51:39 +0100

    Implemented Social Encounters

--- src/Modules/Encounters/Explorer.Encounters.API/Dtos/EncounterDto.cs
+++ src/Modules/Encounters/Explorer.Encounters.API/Dtos/EncounterDto.cs
@@ -43,11 +43,11 @@ namespace Explorer.Encounters.API.Dtos
         public bool? IsRequired { get; set; }
         public int? TourId { get; set; }
         public double ActivateRange { get; set; }
-        public string Image { get; set; }
-        public double ImageLongitude { get; set; }
-        public double ImageLatitude { get; set; }
-        public string Instructions { get; set; }
-        public int PeopleNumb { get; set; }
+        public string? Image { get; set; }
+        public double? ImageLongitude { get; set; }
+        public double? ImageLatitude { get; set; }
+        public string? Instructions { get; set; }
+        public int? PeopleNumb { get; set; }
 
     }
 }

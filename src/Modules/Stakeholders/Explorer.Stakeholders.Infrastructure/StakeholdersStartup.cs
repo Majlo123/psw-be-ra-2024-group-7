3e13f63c8000e390148c18414a0c13b946d75915
nenad <nenaddubovac2105@gmail.com>
2024-11-24 23:28:14 +0100

    feat/Tourist club messages implemented + tests

--- src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/StakeholdersStartup.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/StakeholdersStartup.cs
@@ -34,6 +34,7 @@ public static class StakeholdersStartup
         services.AddScoped<INotificationService, NotificationService>();
        
         services.AddScoped<IProfileMessageService, ProfileMessageService>();
+        services.AddScoped<IClubMessageService, ClubMessageService>();
 
         services.AddScoped<IToursitClubService, TouristClubService>();
 
@@ -56,6 +57,9 @@ public static class StakeholdersStartup
         //services.AddScoped(typeof(ICrudRepository<ProfileMessage>), typeof(CrudDatabaseRepository<ProfileMessage, StakeholdersContext>));
         services.AddScoped<ICrudRepository<ProfileMessage>, CrudDatabaseRepository<ProfileMessage, StakeholdersContext>>();
         services.AddScoped<IProfileMessageRepository, ProfileMessageRepository>();
+       
+        services.AddScoped<ICrudRepository<ClubMessage>, CrudDatabaseRepository<ClubMessage, StakeholdersContext>>();
+        services.AddScoped<IClubMessageRepository, ClubMessageRepository>();
 
         services.AddScoped(typeof(ICrudRepository<TourProblemReport>), typeof(CrudDatabaseRepository<TourProblemReport, StakeholdersContext>));
         services.AddScoped<ITourProblemReportRepository, TourProblemReportRepository>();

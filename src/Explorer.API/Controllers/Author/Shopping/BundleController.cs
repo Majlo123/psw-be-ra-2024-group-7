2f27e0fa2a9cce5813f0a6d8e4a15c6f06573620
MilosNesh <neskovic.milos02@gmail.com>
2024-11-25 20:00:58 +0100

    feat: implemented creation, updating and deletion of tour bundle

---
+++ src/Explorer.API/Controllers/Author/Shopping/BundleController.cs
@@ -0,0 +1,50 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Shopping.API.Dtos;
+using Explorer.Shopping.API.Public;
+using Explorer.Stakeholders.Infrastructure.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Author.Shopping;
+
+[Route("api/administration/bundle")]
+[Authorize(Policy = "authorPolicy")]
+public class BundleController : BaseApiController
+{
+    private readonly IBundleService _bundleService;
+
+    public BundleController(IBundleService bundleService)
+    {
+        _bundleService = bundleService;
+    }
+
+    [HttpPost]
+    public ActionResult<BundleDto> Create([FromBody] BundleDto bundleDto)
+    {
+        bundleDto.CreatorId = this.User.PersonId();
+        var result = _bundleService.Create(bundleDto);
+        return CreateResponse(result);
+    }
+    [HttpPut("{id:int}")]
+    public ActionResult<BundleDto> Update([FromBody] BundleDto bundleDto)
+    {
+        if(bundleDto.CreatorId != this.User.PersonId())
+            return Unauthorized("This is not your bundle!");
+        var result = _bundleService.Update(bundleDto);
+        return CreateResponse(result);
+    }
+    [HttpDelete("{id:long}")]
+    public ActionResult Delete(long id)
+    {
+        if (_bundleService.Get(id).Value.CreatorId != this.User.PersonId())
+            return Unauthorized("This is not your bundle!");
+        var result = _bundleService.Delete(id);
+        return CreateResponse(result);
+    }
+    [HttpGet]
+    public ActionResult<PagedResult<BundleDto>> GetMyBundles([FromQuery] int page, [FromQuery] int pageSize)
+    {
+        var result = _bundleService.GetPagedByCreatorId(this.User.PersonId(), page, pageSize); 
+        return CreateResponse(result);
+    }
+}
---
+++ src/Modules/Shopping/Explorer.Shopping.Infrastructure/Database/Repository/BundleRepository.cs
@@ -0,0 +1,64 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.BuildingBlocks.Infrastructure.Database;
+using Explorer.Shopping.Core.Domain;
+using Explorer.Shopping.Core.Domain.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Shopping.Infrastructure.Database.Repository;
+
+public class BundleRepository : IBundleRepository
+{
+    private readonly ShoppingContext _shoppingContext;
+    private readonly DbSet<Bundle> _dbSet;
+
+    public BundleRepository(ShoppingContext shoppingContext)
+    {
+        _shoppingContext = shoppingContext;
+        _dbSet = shoppingContext.Set<Bundle>();
+    }
+
+    public Bundle Create(Bundle bundle)
+    {
+        _dbSet.Add(bundle);
+        _shoppingContext.SaveChanges();
+        return bundle;
+    }
+
+    public void Delete(long id)
+    {
+        var entity = _dbSet.FirstOrDefault(b => b.Id == id);
+        _dbSet.Remove(entity);
+        _shoppingContext.SaveChanges();
+    }
+
+    public Bundle Get(long id)
+    {
+        return _dbSet.Include(b => b.Products).FirstOrDefault(b => b.Id == id);
+    }
+
+    public PagedResult<Bundle> GetPaged(int page, int pageSize)
+    {
+        var task = _dbSet.Include(b => b.Products).GetPagedById(page, pageSize);
+        task.Wait();
+        return task.Result;
+    }
+
+    public Bundle Update(Bundle bundle)
+    {
+        _shoppingContext.Update(bundle);
+        _shoppingContext.SaveChanges();
+        return bundle;
+    }
+
+    public PagedResult<Bundle> GetPagedByCreatorId(long creatorId, int page, int pageSize)
+    {
+        var task = _dbSet.Include(b => b.Products).Where(b => b.CreatorId == creatorId).GetPagedById(page, pageSize);
+        task.Wait();
+        return task.Result;
+    }
+}
